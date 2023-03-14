3efcb122979a6facf8472a762b1eefca8df8abda
hanyi <holahanyi@gmail.com>
2023-03-14 18:05:37 +0900

    03/14 practice

--- src/main/java/p0314/Solution2.java
+++ src/main/java/p0314/Solution2.java
@@ -1,6 +1,24 @@
 package p0314;
 
+
 public class Solution2 {
 
-    //
+    //소수 찾기
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i=2; i<=n; i++){
+            boolean flag = true;
+            for(int j=2; j<=Math.sqrt(i); j++){
+                if(i%j == 0){
+                    flag = false;
+                    break;
+                }
+            }//for end
+            if(flag == true) answer++;
+        }//for end
+
+        return answer;
+    }
+
 }//class end
