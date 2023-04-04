e2a1f79eb7696247a2f78c9d827625c9962a5882
hanyi <holahanyi@gmail.com>
2023-04-04 09:01:56 +0900

    practice

--- src/main/java/level2/p04/Solution0404.java
+++ src/main/java/level2/p04/Solution0404.java
@@ -1,6 +1,25 @@
 package level2.p04;
 
+import java.util.HashMap;
+
 public class Solution0404 {
 
-    //
+    //위장
+    public int solution(String[][] clothes) {
+        int answer = 1;
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        //의상 종류별 개수 카운트
+        for(int i=0; i< clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 1) + 1);
+        }
+
+        //경우의 수 구하기
+        for(String key : map.keySet()){
+            answer *= map.get(key);
+        }
+
+        return answer - 1;
+    }
 }
